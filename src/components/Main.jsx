a5573dc0879e9e1f157d856865055f40311a41da
Swarna Jang Maden <limbujungmaden@gmail.com>
2023-12-09 18:42:53 +0545

    create main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,16 @@
 function Main() {
-  return <h1>MAIn goes here</h1>;
+  return (
+    <div className="main">
+      <h1>Fun facts about React</h1>
+      <ul className="main--facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Main;
